053524374dcc0f9b8c0a0f13b57ded229abcb69b
unknown <haipro2141999@gmail.com>
2023-02-13 10:08:09 +0700

    master

---
+++ resources/views/BACKEND/Layout/form.blade.php
@@ -0,0 +1,35 @@
+<main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Form Editors</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item">Forms</li>
+          <li class="breadcrumb-item active">Editors</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">TinyMCE Editor</h5>
+
+              <!-- TinyMCE Editor -->
+              <textarea class="tinymce-editor">
+                <p>Hello World!</p>
+                <p>This is TinyMCE <strong>full</strong> editor</p>
+              </textarea><!-- End TinyMCE Editor -->
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main>
